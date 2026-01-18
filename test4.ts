f3b51dd0488f175b814ae9bb83fd77ff82234e99
Abbas <syed.abbas00@gmail.com>
2026-01-18 10:08:22 +0400

    second commid

---
+++ test4.ts
@@ -0,0 +1,21 @@
+//var i:number=1;
+//while(i<=10)
+//{
+ //console.log(i);
+ //i++;
+//}
+
+// while(i<=10)
+// {
+//     console.log(i);
+//     i+=2;
+// }
+
+var i:number=10;
+while(i>=0)
+{
+    console.log(i);
+    i--;
+}
+
+
